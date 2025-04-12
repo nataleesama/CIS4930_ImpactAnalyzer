50b039151edd1c3a077f962cb4e41bbaeb5a8c15
Natalee Sama <nataleesama@gmail.com>
2025-04-11 23:00:33 -0400

    Updated Tests

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -8,33 +8,25 @@ from data_processor import DataProcessor
 
 class TestAlgorithms(unittest.TestCase):
     def setUp(self):
-        self.test_file = "../data/tallahasseeData.json"
+        self.test_file = "../data/test_data.json"
         self.processor = DataProcessor(self.test_file)
         self.x, self.y = self.processor.get_features_and_target()
         self.dates = self.x.tolist()
-        print(f"Dates: {self.dates}\n\n")
         self.precipitations = self.y.tolist()
-        print(f"Precipitations: {self.precipitations}\n\n")
+        self.test_model = CustomPrecipitationPredictor()
 
     def test_precipitation_detector(self):
-        test_model = CustomPrecipitationPredictor()
-        test_model.fit(self.x, self.y)
-        test_prediction = test_model.predict(self.x)
+        self.test_model.fit(self.x, self.y)
+        test_prediction = self.test_model.predict(self.x)
 
-        # Check Success/Accuracy of Predictor
-        # Ensure Results are within 1 unit of real value
-        #assert np.allclose(test_prediction, self.precipitations, atol=1.0)
-        print("Predictions display acceptable accuracy.")
+        print("Predictions may be slightly skewed due to small dataset.")
         for r, p in zip(self.y, test_prediction):
             print(f"Real: {r}, Predicted: {p}")
-"""
-    def test_detect_anomalies(self):
-        # change typical threshold to compensate for small dataset
-        test_model = CustomPrecipitationPredictor()
-        test_model.fit(self.dates, self.precipitations)
-        anomalies = test_model.detect_anomalies(self.precipitations, window_size=3, threshold=0.5)
-        print(f"Anamolies:\n", anomalies)
 
+    def test_detect_anomalies(self):
+        anomalies = self.test_model.detect_anomalies(self.x, self.y)[0]
+        print(f"Anamolies:\n", anomalies)
+"""
     def test_custom_clustering(self):
         data = np.array([[1], [2], [100], [101]])
         labels = custom_clustering(data, n_clusters=2)
--- tests/test_visualizer.py
+++ tests/test_visualizer.py
@@ -5,6 +5,7 @@ import sys
 import os
 sys.path.append(os.path.abspath('../src'))
 from visualizer import Visualizer
+from algorithms import CustomPrecipitationPredictor
 
 class TestVisualizer(unittest.TestCase):
     def setUp(self):
@@ -18,12 +19,7 @@ class TestVisualizer(unittest.TestCase):
         self.time_series = [1.0, 2.0, 1.5, 10.0, 2.0, 1.8, 9.5]  
         self.anomalies = [False, False, False, True, False, False, True]
     #If graphs dont need to be displayed during testing, we add flag show_plots to all test functions
-    '''    
-    def tearDown(self):
-        if self.show_plots:
-            plt.show()
-        plt.close('all')
-    '''
+
     def test_plot_precipitation_trend(self):
         
         Visualizer.plot_precipitation_trend(self.years, self.precipitations, self.predictions)
@@ -32,18 +28,40 @@ class TestVisualizer(unittest.TestCase):
     def test_plot_clustered_data(self):
         
         Visualizer.plot_clustered_data(self.cluster_data, self.cluster_labels)
-        
 
-    def test_plot_anomalies(self):
-    
-        Visualizer.plot_anomalies(self.time_series, self.anomalies)
 
     def test_plot_anomalies_no_anomalies(self):
 
-        time_series = [1.0, 2.0, 1.5, 2.0, 1.8]
-        anomalies = [False, False, False, False, False]
+        z1 = [0.5, 2.0, 1.5, 2.0, 1.8]
+        x1 = [0.998633, 1.000000, 0.000000, 0.000684, 0.749829]
+
+        z2 = [0.3, 2.2, 1.7, 1.3, 1.7]
+        x2 = [0.992233, 1.000000, 0.000000, 0.022684, 0.742229]
+
+        z3 = [0.7, 1.8, 1.2, 2.1, 1.0]
+        x3 = [0.933633, 1.000000, 0.000000, 0.003384, 0.743329]
+
+        model = CustomPrecipitationPredictor()
+
+        # Prepare station data dictionary
+        station_data = {
+            'Test 1': {
+                'values': z1,
+                'anomalies': model.detect_anomalies(x1, z1)[0]
+            },
+            'Test 2': {
+                'values': z2,
+                'anomalies': model.detect_anomalies(x2, z2)[0]
+            },
+            'Test 3': {
+                'values': z3,
+                'anomalies': model.detect_anomalies(x3, z3)[0]
+            }
+        }
+
+        # Plot combined boxplot with anomalies
+        Visualizer.plot_anomalies(station_data)
         
-        Visualizer.plot_anomalies(time_series, anomalies)
 
 if __name__ == '__main__':
     unittest.main()
